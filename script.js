2c99eee826551f1d2b47b48f9be69654a29f1b76
AlinaAlexandraC <alina_alexxa97b@yahoo.com>
2025-01-31 19:18:44 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// Anime API: https://otakugifs.xyz/
+
+document.addEventListener("DOMContentLoaded", function () {
+    const randomizeButton = document.querySelector(".randomize");
+    const clearButton = document.querySelector(".clear");
+    const displayNumber = document.querySelector(".display");
+    const maxNumber = document.querySelector(".max-number");
+    const inputError = document.querySelector(".input-error");
+
+    const handleInputValidity = () => {
+        const regex = /^\d+$/;
+        if (maxNumber.value === "" || !regex.test(maxNumber.value)) {
+            inputError.innerHTML = "Input must be a number";
+
+            setTimeout(() => {
+                inputError.innerHTML = "";
+            }, 3000);
+
+            return false;
+        }
+
+        return true;
+    };
+
+    const getRandomNumber = () => {
+        if (!handleInputValidity()) {
+            return;
+        }
+
+        let randomNumber = Math.floor(Math.random() * maxNumber.value);
+        displayNumber.innerHTML = randomNumber;
+    };
+
+    const clearInput = () => {
+        maxNumber.value = "";
+        displayNumber.innerHTML = "-";
+    };
+
+    randomizeButton.addEventListener("click", getRandomNumber);
+    clearButton.addEventListener("click", clearInput);
+});
